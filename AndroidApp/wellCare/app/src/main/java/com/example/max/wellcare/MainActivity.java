317caab2bbd8f57f458b43b665f45379d47129fa
Suyash <suyash.21.more@gmail.com>
2019-01-06 01:06:35 +0530

    Code Cleanup: Code Dosen't Work

--- AndroidApp/wellCare/app/src/main/java/com/example/max/wellcare/MainActivity.java
+++ AndroidApp/wellCare/app/src/main/java/com/example/max/wellcare/MainActivity.java
@@ -173,11 +173,13 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
             Log.i(MYTAG,"Edit Menu");
             changeFragment(0);
+            
 
         } else if (id == R.id.nav_pathalogy) {
 
             Log.i(MYTAG,"Pathalogy");
             changeFragment(1);
+
         } else if (id == R.id.nav_chemist) {
             Log.i(MYTAG,"Chemist");
             changeFragment(2);
