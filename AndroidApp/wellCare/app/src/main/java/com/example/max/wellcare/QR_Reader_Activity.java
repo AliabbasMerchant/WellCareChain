b109975636298bc0083fe15aefcee52343b83f56
Suyash <suyash.21.more@gmail.com>
2019-01-05 20:22:05 +0530

    QR-Scanner Works

--- AndroidApp/wellCare/app/src/main/java/com/example/max/wellcare/QR_Reader_Activity.java
+++ AndroidApp/wellCare/app/src/main/java/com/example/max/wellcare/QR_Reader_Activity.java
@@ -1,11 +1,13 @@
 package com.example.max.wellcare;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -23,20 +25,18 @@ public class QR_Reader_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_qr__reader_);
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull final Result result) {
-//                activity.runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-                Toast.makeText(QR_Reader_Activity.this, result.getText(), Toast.LENGTH_SHORT).show();
-
+                Log.i("DEBUG_UI","Scanning Complete ,Scan Result");
+                Log.i("DEBUG_UI",result.getText());
+                Intent intent = new Intent();
+                intent.putExtra("ScanRes",result.getText());
+                setResult(RESULT_OK,intent);
+                finish();
             }
         });
         scannerView.setOnClickListener(new View.OnClickListener() {
